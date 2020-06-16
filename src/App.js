e8ea206fb7588efb1bf8a2ea22b80ee2dd350b60
Tim Parsons <timmparsons85@gmail.com>
2020-06-15 21:37:17 -0700

    sidebar with styled components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from './components/sidebar/sidebar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
